082480e16bb8147d8f2c7c3db15776884a934f91
Mike Hacker <mhackersu@gmail.com>
2024-01-08 14:44:50 -0600

    menu

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,13 @@ export default function Home() {
       <main>
         <Header title="bfsio.net is up" />
         <p className="description">
-          <code>happy little code</code>
+          <code href="http://fostercs.wpengine.com/">happy little code</code>
+          <code href="https://blog.bfsio.net/">blog</code>
+          <code href="https://netlify-go-function.netlify.app/">go lambda function</code>
+          <code href="https://k8s-debugging-helper-app.netlify.app/">k8s debugging helper</code>
+          <code href="https://mvskokvlke360.wpengine.com/">mvskokvlke</code>
+          <code href="https://bond-flix.netlify.app/">bondflix</code>
+          <code href="https://from-the-kitchen-of.netlify.app/">from the kitchen of</code>
         </p>
       </main>
 
