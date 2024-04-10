6809fc3ff36882f0f20692faf3a1a2a848048cba
Mike Hacker <mhackersu@gmail.com>
2024-04-10 17:07:32 -0500

    update footer home link to img

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,11 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/bfsio-logo.png" alt="Banana For Scale Logo" className={styles.logo} />
-      </footer>
-        <a href="/">
-          <code>home</code>
-        </a>
+      <a href="/">
+        <footer className={styles.footer}>
+          <img src="/bfsio-logo.png" alt="Banana For Scale Logo" className={styles.logo} />
+        </footer>
+      </a>
     </>
   )
 }
