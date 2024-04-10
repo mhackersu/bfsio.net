94e175c97d41b644c4a1a58a982d1907861bf0a8
Mike Hacker <mhackersu@gmail.com>
2024-04-10 18:03:57 -0500

    intro

--- pages/welcome.js
+++ pages/welcome.js
@@ -17,6 +17,10 @@ export default function Welcome() {
 	        <a>
 	          <code>welcome</code>
 	        </a>
+          <a>hello world, I am a software engineer with a background in DevOps and SRE within product spaces primarily around the edge.</a>
+          <a>In additional to being professionally interested in software engineering, I am personally interested to quite some extent as well.</a>
+          <a>I will write about it here, including my ham radio (KI5IBD) and weather interests.</a>
+          <a>My other interests include clarinet, oil painting, and experimenting with my OP-1.</a>
 	      </p>
       </main>
       <Footer />
