721fbacfb6334debff8a87ee32163029d8003013
Mike Hacker <mhackersu@gmail.com>
2024-04-10 17:35:51 -0500

    accordion

--- pages/projects.js
+++ pages/projects.js
@@ -3,47 +3,30 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import {Accordion, AccordionItem} from "@nextui-org/react";
 
 export default function Projects() {
+  const defaultContentA = "A";
+  const defaultContentB = "B";
+  const defaultContentC = "C";
+
   return (
+    <Accordion variant="bordered">
+      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
+        {defaultContentA}
+      </AccordionItem>
+      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
+        {defaultContentB}
+      </AccordionItem>
+      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
+        {defaultContentC}
+      </AccordionItem>
+    </Accordion>
     <div className="container">
       <Head>
         <title>Projects</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <Header title="Projects" />
-          <p className="menu-item">
-            <a href="https://react-js-toggle-lamp.netlify.app/" target='_window'>
-              <code>toggle the lamp</code>
-            </a>
-          </p>
-          <p className="menu-item">
-            <a href="https://bond-flix.netlify.app/" target='_window'>
-              <code>watch movies</code>
-            </a>
-          </p>
-          <p className="menu-item">
-            <a href="https://from-the-kitchen-of.netlify.app/" target='_window'>
-              <code>cook recipes</code>
-            </a>
-          </p>
-          <p className="menu-item">
-            <a href="https://fp-weather.netlify.app/" target='_window'>
-              <code>check the weather report</code>
-            </a>
-          </p>
-          <p className="menu-item">
-            <a href="https://compliment-time.netlify.app/" target='_window'>
-              <code>receive a compliment</code>
-            </a>
-          </p>
-          <p className="menu-item">
-            <a href="https://netlify-go-function.netlify.app/" target='_window'>
-              <code>decode metar messages</code>
-            </a>
-          </p>
-      </main>
       <Footer />
     </div>
   )
