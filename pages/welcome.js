1d4a5d1116a549d2df84b8310b252736ad656fc6
Mike Hacker <mhackersu@gmail.com>
2024-04-10 20:44:11 -0500

    welcome

--- pages/welcome.js
+++ pages/welcome.js
@@ -14,13 +14,18 @@ export default function Welcome() {
       <main>
         <Header title="Welcome" />
 	      <p className="menu-item">
-	        <a>
-	          <code>welcome</code>
+	        <a>welcome
+	         <code>hello world</code>
 	        </a>
-          <a>hello world, I am a software engineer with a background in DevOps and SRE within product spaces primarily around the edge.</a>
-          <a>In additional to being professionally interested in software engineering, I am personally interested to quite some extent as well.</a>
-          <a>I will write about it here, including my ham radio (KI5IBD) and weather interests.</a>
-          <a>My other interests include clarinet, oil painting, and experimenting with my OP-1.</a>
+          <a>This site is dedicated to topics related to software engineering.</a>
+          <a>In addition to being professionally interested in software engineering, I am personally interested to quite some extent as well.</a>
+          <a>I will write about it here, including my ham radio (KI5IBD) and weather radio interests.</a>
+          <a>I am primarily interested in building applications using next-generation cloud products such as Netlify, Render, Cloudflare, and Fastly.</a>
+          <a>I am primarily focused on contiuning to work with GoLang but I also spend time in Ruby, Python, and JS code-bases.</a>
+          <a>Cloudflare Workers and Netlify Functions are very interesting to me at the moment.</a>
+          <a>My other interests include following r/cyberdeck, practicing clarinet, painting with oils, and experimenting with my OP-1.</a>
+          <br>
+          <a>This site is built on Next.js and runs on Netlify.</a>
 	      </p>
       </main>
       <Footer />
