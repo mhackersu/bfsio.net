7d9a1fc38f72abbc61098e4692f8553b6f5ca2c0
Mike Hacker <mhackersu@gmail.com>
2024-04-12 12:49:10 -0500

    formatting welcome page

--- pages/welcome.js
+++ pages/welcome.js
@@ -6,9 +6,6 @@ import Footer from '@components/Footer'
 import {Accordion, AccordionItem} from "@nextui-org/react";
 
 export default function Welcome() {
-  const welcomeContentA = ""
-  const welcomeContentB = ""
-
   return (
     <div className="container">
       <Head>
@@ -17,62 +14,40 @@ export default function Welcome() {
       </Head>
       <p><a href="/">navigate to main page</a></p>
       <main>
-        <Header title="Welcome"/>
-	      <p className="menu-item">
-	        <a>
-	         <code>hello world</code>
-	        </a>
-          <br></br>
-          <br></br>
-          <a>
-          This site is perfectly imperfect as I continue to work with ReactJS and expand ES knowhow.
-          </a>
-          <br></br>
-          <br></br>
-          <a href="https://github.com/mhackersu/bfsio.net" target="_window">Built with Next.js it is</a>
-          <br></br>
-          <br></br>
+        <Header title=""/>
+        <code>hello world</code>
+        <p className="menu-item">
+            <br></br>
+            <a href="https://github.com/mhackersu/bfsio.net" target="_window">This site is built on Next.js</a>
         </p>
-          <Accordion variant="bordered">
-            <AccordionItem key="1" aria-label="Accordion 1" title="personal interests">
-                <br></br>
-                <br></br>
-                <b>Hobbies:</b>
-                <br></br>
-                <br></br>
-                <li>r/meteorology</li>
-                <li>r/hamradio @KI5IBD</li>
-                <li>r/cyberdeck</li>
-                <li>r/clarinet</li>
-                <li>r/oilpainting</li>
-                <li>r/OP1users</li>
-              <p><code><a href="https://www.github.com/mhackersu" target='_window'>follow me on github</a></code></p>
-            </AccordionItem>
-            <AccordionItem key="2" aria-label="Accordion 2" title="professional experience">
-              <p className="menu-item">
-                Professional experience includes GCP, AWS, and Azure, in DevOps and IaC contexts.
-                <br></br>
-                <br></br>
-                My contributions tend to be backend in nature and written in NodeJS, Ruby, and Python.
-                <br></br>
-                <br></br>
-                I am learning Go; I will share some recent Go projects soon.
-                <br></br>
-                <br></br>
-              </p>
-              <p>
-                <code>
-                  <a href="https://www.linkedin.com/in/mike-hacker" target='_window'>follow me on linkedin</a>
-                </code>
-              </p>
-            </AccordionItem>
-          </Accordion>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
+        <br></br>
+        <b>Personal Interests</b>
+        <br></br>
+        <li>r/meteorology</li>
+        <li>r/hamradio @KI5IBD</li>
+        <li>r/cyberdeck</li>
+        <li>r/clarinet</li>
+        <li>r/oilpainting</li>
+        <li>r/OP1users</li>
+        <br></br>
+        <p>
+          <code>
+            <a href="https://www.github/mhackersu" target='_window'>follow me on GitHub</a>
+          </code>
+        </p>
+        <br></br>
+        <br></br>
+        <b>Professional Experience</b>
+        <br></br>
+        <li>GCP, AWS, and Azure</li>
+        <li>Go, Ruby, Python, JS</li>
+        <li>DevOps, IaC</li>
+        <br></br>
+        <p>
+          <code>
+            <a href="https://www.linkedin.com/in/mike-hacker" target='_window'>follow me on LinkedIn</a>
+          </code>
+        </p>
       </main>
       <Footer />
     </div>
