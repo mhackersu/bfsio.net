e1edd2262181ef59ebfbc1a706fa7c10aa47dbdb
Mike Hacker <mhackersu@gmail.com>
2025-03-22 16:39:41 -0500

    update

--- pages/infra-assistant.js
+++ pages/infra-assistant.js
@@ -17,9 +17,9 @@ export default function Feeder() {
       <a href="/projects"><code>back to projects...</code></a>
       <Header title={title}/>
     <main>
-    <p><code><a href="https://k8s-debugging-helper-app.netlify.app/" target="_window">Kubernetes Developer Debugging Assistant</a></code></p>
-        <p>Kate's Butler</p>
-        <p><code><a href="https://github.com/mhackersu/k8s-debuging-helper-app" target="_window">Source</a></code></p>
+    <p><code><a href="https://k8s-debugging-helper-app.netlify.app/" target="_window">Infra Service Templates</a></code></p>
+        <p>Infra Services</p>
+        <p><code><a href="https://github.com/bfsio/inf-svc" target="_window">Source</a></code></p>
         <br></br>
     </main>
     <Footer />
