c48ff632ad66e321ec76fc0c6861c08b530a4d05
Mike Hacker <mhackersu@gmail.com>
2025-03-27 14:44:38 -0500

    updates

--- pages/feeder.js
+++ pages/feeder.js
@@ -17,9 +17,9 @@ export default function Feeder() {
       <a href="/projects"><code>back to projects...</code></a>
       <Header title={title}/>
     <main>
-        <p><code><a href="https://online-feeder.vercel.app" target="_window">GRPC Event Generator</a></code></p>
-        <p>Online Feeder</p>
-        <p><code><a href="https://github.com/mhackersu/bird-feeder" target="_window">Source</a></code></p>
+        <p><code><a href="https://online-feeder.vercel.app" target="_window">Online Event Generator</a></code></p>
+        <p>Go Feeder</p>
+        <p><code><a href="https://github.com/mhackersu/go-feeder" target="_window">Source</a></code></p>
         <br></br>
     </main>
     <Footer />
