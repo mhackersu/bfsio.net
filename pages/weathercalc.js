73d19142a0a9ea88edf1d7597683b84e6c2e6584
Mike Hacker <mhackersu@gmail.com>
2025-03-28 15:16:23 -0500

    updates

--- pages/weathercalc.js
+++ pages/weathercalc.js
@@ -5,8 +5,8 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 import {Accordion, AccordionItem} from "@nextui-org/react";
 
-export default function WeatherApp() {
-  const title = "Weather App";
+export default function WeatherCalc() {
+  const title = "Weather Calc";
   return (
     <div className="container">
       <Head>
@@ -18,13 +18,13 @@ export default function WeatherApp() {
       <a href="/projects"><code>back to projects...</code></a>
       <Header title={title}/>
     <main>
-        <p><code><a href="https://fp-weather.netlify.app/" target='_window'>Weather App</a></code></p>
+        <p><code><a href="https://fp-weather.netlify.app/" target='_window'>Calculate Local Weather</a></code></p>
         <br></br>
-        <li>Utilizes RamdaJS</li>
-        <li>Functional Programming in JS</li>
+        {/* <li>Utilizes RamdaJS</li> */}
+        {/* <li>Functional Programming in JS</li> */}
         <br></br>
-        <p><code><a href="https://github.com/mhackersu/fp-app-openweather-api" target='_window'>Github</a></code></p>
-        <p><code><a href="https://github.com/mhackersu/avwx-api-js" target='_window'>New Backend in Progress</a></code></p>
+        <p><code><a href="https://github.com/mhackersu/weathercalc" target='_window'>Github</a></code></p>
+        {/* <p><code><a href="https://github.com/mhackersu/avwx-api-js" target='_window'>New Backend in Progress</a></code></p> */}
     </main>
     <Footer />
     </div>
