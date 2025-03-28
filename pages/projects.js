c1c72db044d4b611949081f7fa2a5cb2209c6398
Mike Hacker <mhackersu@gmail.com>
2025-03-28 15:12:07 -0500

    updates

--- pages/projects.js
+++ pages/projects.js
@@ -17,11 +17,11 @@ export default function Projects() {
       <a href="/"><code>back to home page...</code></a>
       <Header title={title}/>
       <main>
-        <p><code><a href="/feeder">Online Booking & Registration</a></code></p>
-        <p><code><a href="/sdc">Financial Analysis Calculator</a></code></p>
         <p><code><a href="/weathercalc">Calculate Local Weather</a></code></p>
-        <p><code><a href="/holycalc">Calculate Holy Days</a></code></p>
-        <p><code><a href="/masslookup">Lookup Local Mass</a></code></p>
+        <p><code><a href="/sdc">Financial Analysis Calculator</a></code></p>
+        <p><code><a href="/booking">Online Booking & Registration</a></code></p>
+        {/* <p><code><a href="/holycalc">Calculate Holy Days</a></code></p> */}
+        {/* <p><code><a href="/masslookup">Lookup Local Mass</a></code></p> */}
       </main>
       <Footer />
     </div>
